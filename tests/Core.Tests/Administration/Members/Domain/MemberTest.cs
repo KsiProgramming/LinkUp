e73907cbb7e41541c9f3e6195857312ad34318b0
LaBani Said <labani.said@outlook.com>
2025-10-04 20:09:50 +0200

    Add MemberTest and project reference for core dependency
    Added a project reference to `LinkUp.Core.csproj` in the test
    project to include the core library as a dependency. Introduced
    a new `MemberTest` class in the `LinkUp.Administration.Members.Tests`
    namespace. Added a `Constructor` unit test to validate the
    initialization of the `Member` class with expected values.

---
+++ tests/Core.Tests/Administration/Members/Domain/MemberTest.cs
@@ -0,0 +1,17 @@
+﻿namespace LinkUp.Administration.Members.Tests;
+
+public class MemberTest
+{
+    [Fact]
+    public void Constructor()
+    {
+        var member = new Member(Guid.Parse("11111111-2222-3333-4444-555555555555"), "John Login", "jhon.doe@domain.com", "John", "Doe", "John Doe");
+
+        Assert.Equal(Guid.Parse("11111111-2222-3333-4444-555555555555"), member.Id);
+        Assert.Equal("John Login", member.Login);
+        Assert.Equal("jhon.doe@domain.com", member.Email);
+        Assert.Equal("John", member.FirstName);
+        Assert.Equal("Doe", member.LastName);
+        Assert.Equal("John Doe", member.Name);
+    }
+}
